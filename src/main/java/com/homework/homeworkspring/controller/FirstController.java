ca810361d55724354cf3aa061073ab46fb0ab092
Henelyn <henelyn@gmail.com>
2022-01-28 18:37:15 +0200

    [Change] - added form with inputs. Changed result display

--- src/main/java/com/homework/homeworkspring/controller/FirstController.java
+++ src/main/java/com/homework/homeworkspring/controller/FirstController.java
@@ -2,7 +2,9 @@ package com.homework.homeworkspring.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
     /*The @Controller annotation indicates that a particular class serves the role of a controller.
@@ -17,20 +19,32 @@ import org.springframework.web.bind.annotation.GetMapping;
     WILL GENERATE A SINGLETON WITH ONE INSTANCE FOR LOG */
 public class FirstController {
 
-    @GetMapping("/first-page-with-parameters")
+    @GetMapping("/first-page-with-parameters") // /text is called ENDPOINT
     /*The @GetMapping annotated methods in the @Controller annotated classes handle the
      HTTP GET requests matched with given URI expression.
      BASED ON THAT URL WE GAVE TO @GetMapping ADDRESS THAT/THIS METHOD WILL BE FIRED */
 
 
     public String parameters() { //That String is the name of template
-        log.info("parameters() method is called ");
+        log.info("parameters() method is called "); //that's just an information for log so ill see the method is called
         return "home-page";
     }
 
     @GetMapping("/second-page-converted")
-    public String converted() {
-        log.info("converted() method is called ");
+    public String converted(@RequestParam("firstName") String firstName,
+                            @RequestParam("height") double heightInCm,
+                            @RequestParam("weight") double weightInKg,
+                            Model pageParameters)
+    {
+        log.info("converted() method is called");
+        double convertedHeightInInches = heightInCm * 0.39;
+        double convertedWeightInPounds = weightInKg * 2.2;
+
+        pageParameters.addAttribute("name", firstName)
+                .addAttribute("height", heightInCm)
+                .addAttribute("weight", weightInKg)
+                .addAttribute("heightInInches", convertedHeightInInches)
+                .addAttribute("weightInPounds", convertedWeightInPounds);
         return "converted-page";
     }
 }
